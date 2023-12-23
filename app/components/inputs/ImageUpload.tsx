cd4dc5d43cffcc71e08b5170532deb166486acd4
Fatih HAS <fatihhas2609@gmail.com>
2023-12-24 01:45:38 +0300

    Invidual Listings

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -36,34 +36,35 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
                     <div
                         onClick={() => open?.()}
                         className="
-                relative
-                cursor-pointer
-                hover:opacity-70
-                transition
-                border-dashed
-                border-2
-                p-20
-                border-neutral-300
-                flex
-                flex-col
-                justify-center
-                items-center
-                gap-4
-                text-neutral-600
-                ">
+                            relative
+                            cursor-pointer
+                            hover:opacity-70
+                            transition
+                            border-dashed
+                            border-2
+                            p-20
+                            border-neutral-300
+                            flex
+                            flex-col
+                            justify-center
+                            items-center
+                            gap-4
+                            text-neutral-600
+                            "
+                    >
                         <TbPhotoPlus size={50} />
                         <div className="font-semibold text-lg">
                             Click to Upload
                         </div>
                         {value && (
                             <div className="absolute inset-0 w-full h-full">
-                            <Image
-                            alt="upload"
-                            fill
-                            style={{objectFit:'cover'}}
-                            src={value}
-                            
-                            />
+                                <Image
+                                    alt="upload"
+                                    fill
+                                    style={{ objectFit: 'cover' }}
+                                    src={value}
+
+                                />
                             </div>
                         )}
                     </div>
